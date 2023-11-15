32ba1c507c99fd5bae8d2dac502c45ee28cae91b
yvesls <yveslimasilva@gmail.com>
2023-11-15 12:52:44 -0300

    iniciando migração para vue

---
+++ gestao_vue/gestaoconvidados/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-btn @click="toggleTheme" class="float-right">toggle theme</v-btn>
+    <router-view></router-view>
+</template>
+
+<script setup>
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+
+function toggleTheme () {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+</script>
---
+++ gestao_vue/gestaoconvidados/src/layouts/default/AppBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app-bar flat :elevation="16" scroll-behavior="collapse">
+    <v-app-bar-title>
+      Guest Management
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
